0b5e1c4d5ec365eae814573bbb9763614b9a43e6
appbros44 <appbros44@gmail.com>
2026-03-06 16:20:59 +0530

    Removed unused code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
-    implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-messaging")
 }
